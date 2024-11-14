b7b6304cc25b8fe7836b31cb65b22f4555c19483
Kichelle Y Momohara-Pelous <87834614+KMomoharaPelous@users.noreply.github.com>
2024-11-14 15:36:33 +0000

    Commit updates to the project

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/snow-mountain.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Snowy Mountain Peaks";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Full-stack web developer student studying at Arizona State University. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
